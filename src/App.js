ac0a1c3d0a6370d662b11aee02b37f81bb1f250b
atagiev01 <atagiev925@gmail.com>
2024-04-01 01:50:41 +0400

    Fibonacci_Factorial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+
+  document.body.style.padding = "20px";
+  document.body.style.margin = 0;
+  document.body.style.boxSizing = "border-box";
+  document.body.style.background = "black";
+
+
+  const [fibonacci, setFibonacci] = useState([]);
+  const [factorial, setFactorial] = useState([]);
+
+  useEffect(() => {
+    const calculateFibonacci = (n) => {
+      if (n === 0) return [0];
+      if (n === 1) return [0, 1];
+
+      let a = 0;
+      let b = 1;
+      const result = [0, 1];
+
+      for (let i = 2; i <= n; i++) {
+        const nextFib = a + b;
+        result.push(nextFib);
+        a = b;
+        b = nextFib;
+      }
+      return result; 
+    }
+
+    const calculateFactorial = (n) => {
+      const result = [];
+      let factorial = 1;
+      for (let i = 1; i <= n; i++) {
+        factorial *= i;
+        result.push(factorial);
+      }
+      return result;
+    }
+
+    const fibonacciResult = calculateFibonacci(10); 
+    const factorialResult = calculateFactorial(10);
+    setFibonacci(fibonacciResult);
+    setFactorial(factorialResult);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ backgroundColor: "#003",
+                  color:"white",
+                  padding:"20px",
+                  position: 'absolute',
+                  width:"100%",
+                  maxWidth:"500px",
+                  top:"50%",
+                  left:"50%",
+                  transform:'translate(-50%,-50%)',
+     }}>
+      <div style={{ backgroundColor: "#900000 ", color:"white",padding:"20px",display:'flex'}}>
+          {fibonacci.map((item, index) => (
+              <div key={index} style={{padding:"0px"}}>
+                    {item}{index === fibonacci.length - 1 ? " " : "_"}
+              </div>
+          ))}
+      </div>
+
+      <br></br>
+      <br></br>
+      <br></br>
+
+      <div style={{ backgroundColor: "#007050 ", color:"white",padding:"20px"}}>
+        {
+            factorial.map((item,index)=>(
+                <div key={index}>{index+1 +"! = "+item}</div>
+            ))
+        }
+      </div>
     </div>
   );
 }
